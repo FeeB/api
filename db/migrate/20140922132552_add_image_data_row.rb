5b92c3ef4c0bbabd46f74d6d51376bbfd61f43a7
Braun,Fee <Fee.Braun@brands4friends.de>
2014-09-23 12:28:11 +0200

    delete sql 3 instead of pg

--- db/migrate/20140922132552_add_image_data_row.rb
+++
@@ -1,5 +0,0 @@
-class AddImageDataRow < ActiveRecord::Migration
-  def change
-  	add_column :devices, :image_data, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140922144638) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "devices", force: true do |t|
     t.string   "deviceName"
     t.string   "category"
