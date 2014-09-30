541a2111765624db258f19c1aacf014fdd85ca68
Braun,Fee <Fee.Braun@brands4friends.de>
2014-09-30 12:46:14 +0200

    deleted debugging puts

--- app/models/device.rb
+++ app/models/device.rb
@@ -15,9 +15,7 @@ class Device < ActiveRecord::Base
 		    data.original_filename = self.id.to_s + ".png"
 		    data.content_type = "image/png"
 
-		    self.avatar = data
-		    puts '--------------------------------------'
-		    puts avatar.url
+		    self.avatar = datax
 		    self.image_url = avatar.url(:medium);
 		end
 	end
